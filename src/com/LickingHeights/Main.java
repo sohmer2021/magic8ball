8aece454a9905521c56943d5ef3b402901ec26eb
Sohmer Lawrence <sohmer.lawrence.2021@student.lhschools.org>
2019-02-19 07:49:46 -0500

    int input

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,27 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	/*Without a doubt
+	* Absolutely
+	* The answer is yes
+	* The outlook is good
+	* signs point to yes
+	* the answer is no
+	* doubtful
+	* uncertain
+	* don't count on it
+	* outlook not so good*/
+
+		Scanner keyboard;
+		int firstValue;
+		int secondValue;
+		String input;
+
+		keyboard = new Scanner(System.in);
+		
     }
 }
