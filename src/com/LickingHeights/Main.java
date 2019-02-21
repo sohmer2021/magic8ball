8cf139cfa9c341b9019727bd4558611343feb235
Sohmer Lawrence <sohmer.lawrence.2021@student.lhschools.org>
2019-02-21 07:53:08 -0500

    printed outputs

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-	/*Without a doubt
-	* Absolutely
-	* The answer is yes
-	* The outlook is good
-	* signs point to yes
-	* the answer is no
-	* doubtful
-	* uncertain
-	* don't count on it
-	* outlook not so good*/
+	public static void main(String[] args) {
+		/*Without a doubt
+		 * Absolutely
+		 * The answer is yes
+		 * The outlook is good
+		 * signs point to yes
+		 * the answer is no
+		 * doubtful
+		 * uncertain
+		 * don't count on it
+		 * outlook not so good*/
 
 		/*int firstValue = 1-10;
 		int secondValue = 11-20;
@@ -34,27 +34,25 @@ public class Main {
 		System.out.println("Pick a number between 1 and 100");
 		input = keyboard.nextInt();
 
-		if(input >=1 && input <=10){
+		if (input >= 1 && input <= 10) {
 
-		}
-		else if (input >=11 && input <=20){
+		} else if (input >= 11 && input <= 20) {
+		System.out.println("Without a doubt")
+		} else if (input >= 21 && input <= 30) {
+		System.out.println("Outlook not so good")
+		} else if (input >= 31 && input <= 40) {
+		System.out.println("Absolutely")
+		} else if (input >= 41 && input <= 50) {
+		System.out.println("The answer is yes")
+		} else if (input >= 51 && input <= 60) {
 
-		}
-		else if (input >=21 && input <=30){
+		} else if (input >= 61 && input <= 70) {
 
-		}
-		else if (input >=31 && input <=40){
+		} else if (input >= 71 && input <= 80) {
 
-		}
-		else if (input >=41 && input <=50){
+		} else if (input >= 81 && input <= 90) {
 
+		} else if (input >= 91 && input <= 100) {
 		}
-		else if (input >=51 && input <=60){
-
-		}
-		else if (input >=61 && input <=70){
-
-		}
-		else if (input)
-    }
+	}
 }
